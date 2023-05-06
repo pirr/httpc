0bbfda9bf70cf59f64a7ad9abb6a410f380a9b31
Aleksey Smaga <tattoo1mag@gmail.com>
2023-05-06 21:27:04 +0200

    [fix&tests] fix deleting hashmap elemet, use glib test framework

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,12 +1,12 @@
 #include "hashmap.h"
+#include <stdio.h>
 
 size_t
 hash(const char *key, size_t hash_size)
 {
     size_t hashval;
 
-    for (hashval = 0; *key != '\0'; key++)
-        hashval = *key + 31 * hashval;
+    for (hashval = 0; *key != '\0'; key++) hashval = *key + 31 * hashval;
 
     return hashval % hash_size;
 }
@@ -16,13 +16,12 @@ init_hashmap(size_t size)
 {
     size_t i;
     hashmap_storage_t *hash_map;
-    hash_map = (hashmap_storage_t *) malloc(sizeof(hashmap_storage_t));
+    hash_map = (hashmap_storage_t *)malloc(sizeof(hashmap_storage_t));
     hash_map->size = size;
-    hash_map->storage = (hashmap_element_t **) malloc(sizeof(hashmap_element_t *) * size);
+    hash_map->storage = (hashmap_element_t **)malloc(sizeof(hashmap_element_t *) * size);
     hash_map->used_storage = 0;
-    
-    for (i = 0; i < size; i++)
-        hash_map->storage[i] = NULL;
+
+    for (i = 0; i < size; i++) hash_map->storage[i] = NULL;
 
     return hash_map;
 }
@@ -32,7 +31,7 @@ compare_func(hashmap_element_t *el, const char *key)
 {
     if (el == NULL)
         return 0;
-    
+
     return strcmp(el->key, key);
 }
 
@@ -44,7 +43,7 @@ look_hash_el(hashmap_storage_t *hash_storage, const char *key)
     for (el = hash_storage->storage[hash_val]; el != NULL; el = el->next)
         if (compare_func(el, key) == 0)
             return el;
-    
+
     return NULL;
 }
 
@@ -62,40 +61,42 @@ look_hash_value(hashmap_storage_t *hash_storage, const char *key)
 }
 
 hashmap_value_t *
-create_hash_el_value(void *value, size_t value_size, int (*free_value_func)(void**))
+create_hash_el_value(void *value, size_t value_size, int (*free_value_func)(void **))
 {
-    hashmap_value_t *hashmap_value = (hashmap_value_t *) malloc(sizeof(hashmap_value_t));
+    hashmap_value_t *hashmap_value = (hashmap_value_t *)malloc(sizeof(hashmap_value_t));
     hashmap_value->free_value_func = free_value_func;
     hashmap_value->s = value_size;
-    hashmap_value->v = (void *) malloc(value_size);
+    hashmap_value->v = (void *)malloc(value_size);
     memcpy(hashmap_value->v, value, value_size);
 
     return hashmap_value;
 }
 
 hashmap_element_t *
-add_hash_el(hashmap_storage_t *hash_storage, const char *key, void *value, size_t value_size, int (*free_value_func)(void**))
+add_hash_el(hashmap_storage_t *hash_storage, const char *key, void *value, size_t value_size,
+            int (*free_value_func)(void **))
 {
     hashmap_element_t *el, *existed_el;
     size_t hash_val;
-    
+
     if ((el = look_hash_el(hash_storage, key)) == NULL) {
-        el = (hashmap_element_t *) malloc(sizeof(hashmap_element_t));
-        
-        el->key = (char *) malloc(sizeof(key) * strlen(key));
+        el = (hashmap_element_t *)malloc(sizeof(hashmap_element_t));
+
+        el->key = (char *)malloc(sizeof(key) * strlen(key));
         el->next = NULL;
         memcpy(el->key, key, strlen(key));
 
         hash_val = hash(key, hash_storage->size);
         if ((existed_el = hash_storage->storage[hash_val]) != NULL) {
-            while (existed_el->next != NULL)
-                existed_el = existed_el->next;
+            while (existed_el->next != NULL) existed_el = existed_el->next;
             existed_el->next = el;
-        } else {
+        }
+        else {
             hash_storage->storage[hash_val] = el;
         }
         hash_storage->used_storage += 1;
-    } else {
+    }
+    else {
         el->value->free_value_func(&(el->value->v));
         free(el->value);
     }
@@ -121,26 +122,24 @@ delete_hash_el(hashmap_storage_t **hash_storage, const char *key)
         tmp->value->free_value_func(&(tmp->value->v));
         free(tmp->value);
         tmp->value = NULL;
-        free(tmp);
         (*hash_storage)->used_storage -= 1;
 
         hash_val = hash(key, (*hash_storage)->size);
+
         if ((*hash_storage)->storage[hash_val]->key == NULL) {
             // remove first element
-            if (el == NULL) {
-                free((*hash_storage)->storage[hash_val]);
-                (*hash_storage)->storage[hash_val] = NULL;
-            }
-            (*hash_storage)->storage[hash_val] = el;
-        } 
+            (*hash_storage)->storage[hash_val] = NULL;
+        }
         else if (el == NULL) {
             // remove last element
-            for (el = (*hash_storage)->storage[hash_val]; el->next != tmp && el != NULL; el = el->next)
-                {};
+            for (el = (*hash_storage)->storage[hash_val]; el->next != tmp && el != NULL;
+                 el = el->next) {
+            };
             if (el != NULL) {
                 el->next = NULL;
             }
         }
+        free(tmp);
         tmp = NULL;
     }
 }
@@ -163,14 +162,14 @@ free_hash_storage(hashmap_storage_t **hash_storage)
         while (existed_el != NULL) {
             tmp = existed_el;
             existed_el = existed_el->next;
-            
+
             if (tmp->value != NULL) {
                 tmp->value->free_value_func(&(tmp->value->v));
                 free(tmp->value);
             }
             if (tmp->key != NULL)
                 free(tmp->key);
-            
+
             free(tmp);
             tmp = NULL;
         }
--- tests/hashmap_tests.c
+++ tests/hashmap_tests.c
@@ -1,4 +1,6 @@
+#include <glib.h>
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "../src/hashmap.h"
@@ -11,81 +13,105 @@ char *expected[][2] = {
     {"verylonglonglongverylonglonglongkey5", "verylonglonglongverylonglonglongvalue5"},
 };
 
-typedef struct test_s {
+typedef struct {
+    hashmap_storage_t *hashmap;
+} mapfixture;
+
+typedef struct {
     char *value;
 } test_t;
 
 int
-free_test_s(void **test)
+free_test_t(void **t)
 {
-    if (*test == NULL)
-        return 0;
-    
-    free((*((test_t **) test))->value);
-    free(*test);
-    test = NULL;
-
+    test_t **tt = (test_t **)t;
+    free((*tt)->value);
+    free(*tt);
     return 0;
 }
 
-int
-main()
+void
+hashmap_setup(mapfixture *hmf, gconstpointer test_data)
 {
-    hashmap_storage_t *hashmap;
-    hashmap = init_hashmap(2);
-
+    hmf->hashmap = init_hashmap(2);
     unsigned expected_size = sizeof(expected) / sizeof(expected[0]);
     unsigned i;
-    printf("ADD\n");
+
     for (i = 0; i < expected_size; i++) {
-        test_t *test_value = (test_t *) malloc(sizeof(test_t));
+        test_t *test_value = (test_t *)malloc(sizeof(test_t));
         test_value->value = strdup(expected[i][1]);
-        add_hash_el(hashmap, expected[i][0], test_value, sizeof(test_t), free_test_s);
+        add_hash_el(hmf->hashmap, expected[i][0], test_value, sizeof(test_t), free_test_t);
     }
+}
 
-    assert(hashmap->used_storage == expected_size);
+void
+hashmap_teardown(mapfixture *hmf, gconstpointer test_data)
+{
+    free_hash_storage(&(hmf->hashmap));
+}
 
-    printf("LOOK\n");
+void
+check_keys(hashmap_storage_t const *hm)
+{
+    unsigned i, expected_size;
+    expected_size = sizeof(expected) / sizeof(expected[0]);
     for (i = 0; i < expected_size; i++) {
-        test_t *test_value = (test_t *) look_hash_el(hashmap, expected[i][0])->value->v;
-        assert(strcmp(test_value->value, expected[i][1]) == 0);
+        test_t *test_value =
+            (test_t *)look_hash_el((hashmap_storage_t *)hm, expected[i][0])->value->v;
+        g_assert_cmpstr(test_value->value, ==, expected[i][1]);
     }
-    assert(look_hash_el(hashmap, "key1000") == NULL);
+}
 
-    printf("REPLACE\n");
-    test_t *changed_test_value = (test_t *) malloc(sizeof(test_t));
-    changed_test_value->value = strdup("changed value3");
-    add_hash_el(hashmap, "key3", changed_test_value, sizeof(changed_test_value), free_test_s);
-    test_t *changed_test_v = (test_t *) look_hash_el(hashmap, "key3")->value->v;
-    assert(strcmp(changed_test_v->value, changed_test_value->value) == 0);
+void
+update_value(hashmap_storage_t *hm)
+{
+    char *new_value_str = "hello I have been changed.";
+    test_t *changed_value = (test_t *)malloc(sizeof(test_t));
+    changed_value->value = strdup(new_value_str);
+    add_hash_el(hm, expected[0][0], changed_value, sizeof(test_t), free_test_t);
+    test_t *test_value = (test_t *)look_hash_value(hm, expected[0][0]);
+    g_assert_cmpstr(test_value->value, ==, new_value_str);
+}
 
-    printf("DELETE\n");
-    delete_hash_el(&hashmap, "key1");
-    assert(look_hash_el(hashmap, "key1") == NULL);
-    assert(hashmap->used_storage == expected_size - 1);
-    delete_hash_el(&hashmap, "key2");
-    assert(look_hash_el(hashmap, "key2") == NULL);
-    assert(hashmap->used_storage == expected_size - 2);
-    delete_hash_el(&hashmap, "verylonglonglongverylonglonglongkey5");
-    assert(look_hash_el(hashmap, "verylonglonglongverylonglonglongkey5") == NULL);
-    assert(hashmap->used_storage == expected_size - 3);
-    delete_hash_el(&hashmap, "key1000");
-    assert(hashmap->used_storage == expected_size - 3);
-    assert(look_hash_value(hashmap, "key1000") == NULL);
+void
+delete_key(hashmap_storage_t *hm)
+{
+    unsigned i, expected_size;
+    expected_size = sizeof(expected) / sizeof(expected[0]);
+    for (i = 0; i < expected_size; i++) {
+        delete_hash_el(&hm, expected[i][0]);
+    }
+    for (i = 0; i < expected_size; i++) {
+        g_assert_true(look_hash_value(hm, expected[i][0]) == NULL);
+    }
+}
 
-    printf("CHECK AFTER DELETION\n");
-    test_t *el4 = (test_t *) look_hash_el(hashmap, "key4")->value->v;
-    assert(strcmp(el4->value, expected[3][1]) == 0);
+void
+test_new(mapfixture *hmf, gconstpointer ignored)
+{
+    check_keys(hmf->hashmap);
+}
 
-    test_t *el3 = (test_t *) look_hash_el(hashmap, "key3")->value->v;
-    assert(strcmp(el3->value, changed_test_value->value) == 0);
-    test_t *value3 = (test_t *) look_hash_value(hashmap, "key3");
-    assert(strcmp(value3->value, changed_test_value->value) == 0);
+void
+test_update(mapfixture *hmf, gconstpointer ignored)
+{
+    update_value(hmf->hashmap);
+}
 
-    assert(hashmap->storage[0]->next == NULL);
-    assert(hashmap->storage[1]->next == NULL);
+void
+test_delete(mapfixture *hmf, gconstpointer ignored)
+{
+    delete_key(hmf->hashmap);
+}
 
-    printf("FREE\n");
-    free_hash_storage(&hashmap);
-    assert(hashmap == NULL);
+int
+main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+    g_test_add("/set1/new test", mapfixture, NULL, hashmap_setup, test_new, hashmap_teardown);
+    g_test_add("/set1/update value test", mapfixture, NULL, hashmap_setup, test_update,
+               hashmap_teardown);
+    g_test_add("/set1/delete key test", mapfixture, NULL, hashmap_setup, test_delete,
+               hashmap_teardown);
+    return g_test_run();
 }
