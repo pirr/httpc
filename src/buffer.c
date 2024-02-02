0d48400c19c1bf457ac2cac21f8e42fb3cd6dba7
al_smaga <tattoo1mag@gmail.com>
2024-02-02 20:03:24 +0100

    [fixes] fix buffer append (#6)
    Co-authored-by: Aleksey Smaga <al.smaga@proton.me>

--- src/buffer.c
+++ src/buffer.c
@@ -1,16 +1,15 @@
 #include "buffer.h"
 
-
 buffer_t *
 buffer_alloc(int size)
 {
     buffer_t *buf;
-    if ((buf = (buffer_t *) malloc(sizeof(buffer_t))) == NULL) {
+    if ((buf = (buffer_t *)malloc(sizeof(buffer_t))) == NULL) {
         fprintf(stderr, "Buffer allocation error");
         return NULL;
     }
 
-    if ((buf->content = (char *) malloc((size + 1) * sizeof(char))) == NULL) {
+    if ((buf->content = (char *)malloc((size + 1) * sizeof(char))) == NULL) {
         fprintf(stderr, "Buffer content allocation error");
         return NULL;
     }
@@ -28,7 +27,7 @@ __buffer_expand(buffer_t *buf, int new_size)
         return 0;
     }
 
-    if ((buf->content = (char *) realloc(buf->content, new_size)) == NULL) {
+    if ((buf->content = (char *)realloc(buf->content, new_size)) == NULL) {
         return -1;
     }
 
@@ -42,7 +41,7 @@ buffer_append(buffer_t *buf, char *append, int size)
     int allocated, additional;
     allocated = buf->total_size - buf->bytes_used;
     if (allocated < size) {
-        additional = size - allocated;
+        additional = size - allocated + 1;
         if (__buffer_expand(buf, buf->total_size + additional) != 0) {
             return -1;
         }
